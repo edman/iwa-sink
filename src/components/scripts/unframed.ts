50d519a0595e47509fd22f2b7112756956bb6b4f
Edman Anjos <edmanp@google.com>
2026-06-05 13:19:45 +0000

    Use unframed instead of borderless

--- src/components/scripts/unframed.ts
+++ src/components/scripts/unframed.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-document.getElementById('borderless').addEventListener('click', async (e) => {
+document.getElementById('unframed').addEventListener('click', async (e) => {
   e.preventDefault();
   const screenDetails = await window.getScreenDetails();
   console.log(screenDetails);
